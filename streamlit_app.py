cef4857071d689b8f49e661ebacc3e04a6f8cbc6
Aliya <69137998+AliyaAzeez@users.noreply.github.com>
2023-08-08 20:18:48 +0100

    Update streamlit_app.py
    Cleaning the data table

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,11 +24,12 @@ streamlit.dataframe(fruits_to_show)
 
 streamlit.header('Fruityvice Fruit Advice!')
 # python package library requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+# streamlit.text(fruityvice_response.json())
 
 # json version of the response converted to normalize it
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+
 # output as a table
 streamlit.dataframe(fruityvice_normalized)
 
