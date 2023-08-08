2c3759c16a90d11685630ec5884cd44abe2e7623
Aliya <69137998+AliyaAzeez@users.noreply.github.com>
2023-08-08 11:08:13 +0100

    Update streamlit_app.py
    Added another header

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,3 +7,5 @@ streamlit.text('🥣 Omega 3 and blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach and Rocket smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
+
+streamlit.header('🍌🥭 Build your Own Fruit Smoothie 🥝 🍇')
