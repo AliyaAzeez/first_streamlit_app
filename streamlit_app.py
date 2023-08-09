b6cd6e9e04935ebcb05ab20faf2407c18614c6e1
Aliya <69137998+AliyaAzeez@users.noreply.github.com>
2023-08-09 18:19:09 +0100

    Update streamlit_app.py
    resolved minor error

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 import pandas
 import requests
 import snowflake.connector
-import urllib.error import URLError
+import urllib.error from URLError
 
 streamlit.title("My MOM's new healthy Diner")
 
