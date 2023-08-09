32d6b166c6de2a1dfcb8f1a3bff5a11c84960c9b
Aliya <69137998+AliyaAzeez@users.noreply.github.com>
2023-08-09 18:29:38 +0100

    Update streamlit_app.py
    try catch exception added

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,18 +25,23 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 streamlit.header('Fruityvice Fruit Advice!')
-# fruit choice
-fruit_choice = streamlit.text_input('What fruit would you like to know about?','Kiwi')
-streamlit.write('User entered',fruit_choice)
-# python package library requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-# streamlit.text(fruityvice_response.json())
-
-# json version of the response converted to normalize it
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-
-# output as a table
-streamlit.dataframe(fruityvice_normalized)
+try:
+  # fruit choice
+  fruit_choice = streamlit.text_input('What fruit would you like to know about?')
+  if not fruit_choice:
+    streamline.error("Please select a fruit to get information')
+  else:
+    # python package library requests
+    # streamlit.text(fruityvice_response.json())
+    # streamlit.write('User entered',fruit_choice)
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    # json version of the response converted to normalize it
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    # output as a table
+    streamlit.dataframe(fruityvice_normalized)
+except URLError as e:
+  streamlit.error()
+    
 
 # don't run anything past here while we troubleshoot
 streamlit.stop()
